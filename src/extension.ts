aff74bdcdc4ad475cdc8ab6a24541d8112edbd72
Minh Le <minh@hatchways.io>
2023-10-06 19:49:49 -0400

    Run npx prettier

--- src/extension.ts
+++ src/extension.ts
@@ -88,55 +88,52 @@ export function activate(context: vscode.ExtensionContext) {
 
   // When user click on a line
   let disposableCursorDecorations: any = {};
-  socket.on(
-    CURSOR_MOVE,
-    (data) => {
-      const { userPerformingThisAction, cursorPosition, allOnlineUsers } = data;
-    
-      // Remove previous cursor because the user is moving onto a new file now.
-      if (userPerformingThisAction in disposableCursorDecorations) {
-        disposableCursorDecorations[userPerformingThisAction].dispose();
-      }
+  socket.on(CURSOR_MOVE, (data) => {
+    const { userPerformingThisAction, cursorPosition, allOnlineUsers } = data;
 
-      let startPosition: vscode.Position = new vscode.Position(
-        cursorPosition["startLine"],
-        cursorPosition["startCharacter"]
-      );
-      let endPosition: vscode.Position = new vscode.Position(
-        cursorPosition["endLine"],
-        cursorPosition["endCharacter"]
-      );
-
-      const user = allOnlineUsers[userPerformingThisAction];
-      const { r, g, b } = user?.color;
-      const hex =
-        "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
-
-      const activeEditor = vscode.window.activeTextEditor;
-      const cursorDecoration = vscode.window.createTextEditorDecorationType({
-        backgroundColor: `rgba(${r}, ${g}, ${b}, 0.3)`,
-        border: `2px solid ${hex}`,
-      });
-
-      if (cursorPosition["filePath"] !== activeEditor?.document.uri.path) {
-        vscode.window.showTextDocument(
-          vscode.Uri.file(cursorPosition["filePath"]),
-          {
-            viewColumn: vscode.ViewColumn.One,
-          }
-        );
-      }
-
-      activeEditor?.setDecorations(cursorDecoration, [
-        {
-          hoverMessage: user?.name,
-          range: new vscode.Range(startPosition, endPosition),
-        },
-      ]);
-
-      disposableCursorDecorations[userPerformingThisAction] = cursorDecoration;
+    // Remove previous cursor because the user is moving onto a new file now.
+    if (userPerformingThisAction in disposableCursorDecorations) {
+      disposableCursorDecorations[userPerformingThisAction].dispose();
     }
-  );
+
+    let startPosition: vscode.Position = new vscode.Position(
+      cursorPosition["startLine"],
+      cursorPosition["startCharacter"]
+    );
+    let endPosition: vscode.Position = new vscode.Position(
+      cursorPosition["endLine"],
+      cursorPosition["endCharacter"]
+    );
+
+    const user = allOnlineUsers[userPerformingThisAction];
+    const { r, g, b } = user?.color;
+    const hex =
+      "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+
+    const activeEditor = vscode.window.activeTextEditor;
+    const cursorDecoration = vscode.window.createTextEditorDecorationType({
+      backgroundColor: `rgba(${r}, ${g}, ${b}, 0.3)`,
+      border: `2px solid ${hex}`,
+    });
+
+    if (cursorPosition["filePath"] !== activeEditor?.document.uri.path) {
+      vscode.window.showTextDocument(
+        vscode.Uri.file(cursorPosition["filePath"]),
+        {
+          viewColumn: vscode.ViewColumn.One,
+        }
+      );
+    }
+
+    activeEditor?.setDecorations(cursorDecoration, [
+      {
+        hoverMessage: user?.name,
+        range: new vscode.Range(startPosition, endPosition),
+      },
+    ]);
+
+    disposableCursorDecorations[userPerformingThisAction] = cursorDecoration;
+  });
 
   // When user click on a file, send it to Socket
   context.subscriptions.push(
@@ -147,23 +144,22 @@ export function activate(context: vscode.ExtensionContext) {
       }
     })
   );
-  
+
   context.subscriptions.push(
     vscode.window.onDidChangeTextEditorSelection((event) => {
       // Send new cursor position
-        socket.emit(CURSOR_MOVE, {
-          sessionId,
-          cursorPosition: {
-            startLine: event.selections[0].start.line,
-            startCharacter: event.selections[0].start.character,
-            endLine: event.selections[0].end.line,
-            endCharacter: event.selections[0].end.character,
-            filePath: event.textEditor.document.uri.path,
-          },
-        });
+      socket.emit(CURSOR_MOVE, {
+        sessionId,
+        cursorPosition: {
+          startLine: event.selections[0].start.line,
+          startCharacter: event.selections[0].start.character,
+          endLine: event.selections[0].end.line,
+          endCharacter: event.selections[0].end.character,
+          filePath: event.textEditor.document.uri.path,
+        },
+      });
     })
   );
-
 }
 
 // This method is called when your extension is deactivated
